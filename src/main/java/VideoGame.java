02e03fbc5dabf9532403e5aad0700011aa0c55b9
Pierre <pierre.mallevaey@laposte.net>
2018-09-05 17:14:32 +0200

    ajout de VideoGame

---
+++ src/main/java/VideoGame.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+public class VideoGame extends CulturalContent{
+	
+	private List<String> platform;
+	private int numPlayer;
+
+	
+	/*
+	 * *********CONSTRUCTORS
+	 */	
+	
+	public VideoGame(List<String> platform, int numPlayer) {
+		super();
+		this.platform = platform;
+		this.numPlayer = numPlayer;
+	}
+
+	
+	/*
+	 * *********GETTERS/SETTERS
+	 */
+	
+
+	public List<String> getPlatform() {
+		return platform;
+	}
+
+
+	public void setPlatform(List<String> platform) {
+		this.platform = platform;
+	}
+
+
+	public int getNumPlayer() {
+		return numPlayer;
+	}
+
+
+	public void setNumPlayer(int numPlayer) {
+		this.numPlayer = numPlayer;
+	}	
+	
+}
