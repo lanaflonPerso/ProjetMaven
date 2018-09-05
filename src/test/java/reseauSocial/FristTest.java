4201cc4cf2dd390543dbab397b02ebbac08a6dc7
Pierre <pierre.mallevaey@laposte.net>
2018-09-05 14:52:47 +0200

    add structure

---
+++ src/test/java/reseauSocial/FristTest.java
@@ -0,0 +1,14 @@
+package reseauSocial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FristTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
