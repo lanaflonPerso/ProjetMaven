ed37232306532e6eb70405b8135cfa7dbe28ee75
vianneyba <vianneyba@free.fr>
2018-09-06 11:02:20 +0200

    Ajout beans Episode

---
+++ src/main/java/fr/dawan/reseauSoc/beans/Episode.java
@@ -0,0 +1,23 @@
+package fr.dawan.reseauSoc.beans;
+
+public class Episode {
+
+	private int season;
+	private int number;
+	
+	/*
+	 * *********GETTERS/SETTERS
+	 */
+	public int getSeason() {
+		return season;
+	}
+	public void setSeason(int season) {
+		this.season = season;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
