707f4e430bfb78f9c658a98e9c22f364a3bc5706
Pierre <pierre.mallevaey@laposte.net>
2018-09-06 11:34:06 +0200

    ajout javadoc et merge

--- src/main/java/fr/dawan/reseauSoc/beans/VideoGame.java
+++ src/main/java/fr/dawan/reseauSoc/beans/VideoGame.java
@@ -1,9 +1,15 @@
 package fr.dawan.reseauSoc.beans;
 import java.util.List;
 
+/**
+ * The Class VideoGame.
+ */
 public class VideoGame extends CulturalContent{
 	
+	/** plateforme sur lesquels le jeux est disponible ( pc , xbon, ps4 ...). */
 	private List<String> platform;
+	
+	/**nombre de joueur */
 	private int numPlayer;
 
 	
@@ -11,10 +17,19 @@ public class VideoGame extends CulturalContent{
 	 * *********CONSTRUCTORS
 	 */	
 	
+	/**
+	 * Instantiates a new video game.
+	 */
 	public VideoGame() {
 		
 	}
 	
+	/**
+	 * Instantiates a new video game.
+	 *
+	 * @param platform the platform
+	 * @param numPlayer nombre de joueurs
+	 */
 	public VideoGame(List<String> platform, int numPlayer) {
 		super();
 		this.platform = platform;
@@ -27,21 +42,41 @@ public class VideoGame extends CulturalContent{
 	 */
 	
 
+	/**
+	 * Gets the platform.
+	 *
+	 * @return the platform
+	 */
 	public List<String> getPlatform() {
 		return platform;
 	}
 
 
+	/**
+	 * Sets the platform.
+	 *
+	 * @param platform the new platform
+	 */
 	public void setPlatform(List<String> platform) {
 		this.platform = platform;
 	}
 
 
+	/**
+	 * Gets the num player.
+	 *
+	 * @return the num player
+	 */
 	public int getNumPlayer() {
 		return numPlayer;
 	}
 
 
+	/**
+	 * Sets the num player.
+	 *
+	 * @param numPlayer the new num player
+	 */
 	public void setNumPlayer(int numPlayer) {
 		this.numPlayer = numPlayer;
 	}	
