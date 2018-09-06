ea3cc7695ec9c60bc7afd2b66612f9af54423fff
Pierre <pierre.mallevaey@laposte.net>
2018-09-06 12:14:51 +0200

    correction imports

---
+++ src/main/java/fr/dawan/reseauSoc/Util/Launcher.java
@@ -0,0 +1,30 @@
+package fr.dawan.reseauSoc.Util;
+
+import java.util.Scanner;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("bienvenue ds votre reseau social");
+		System.out.println("a tout moment le mot \"menu\" vous permet de revenir au menu principal");
+		System.out.println("entrez votre choix : ");
+		System.out.println("1) consulter la base");
+		System.out.println("2) entrer un contenu ds la base");
+		Scanner sc = new Scanner(System.in);
+		if(sc.nextLine().equals("1")) {
+			System.out.println("a dev");
+		}else if(sc.nextLine().equals("2")) {
+			switch (key) {
+			case value:
+				
+				break;
+
+			default:
+				break;
+			}
+		}
+		
+		sc.close();
+	}
+
+}
