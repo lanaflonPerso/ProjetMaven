fe4ec71a00af68f293d0f8e554599ea6362c81d4
vianneyba <vianneyba@free.fr>
2018-09-14 15:36:16 +0200

    Ajout du filtre de redirection pour les utilisateurs non enregitrer

---
+++ src/main/java/fr/dawan/reseauSoc/filter/ConnectionUser.java
@@ -0,0 +1,39 @@
+package fr.dawan.reseauSoc.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ConnectionUser implements Filter {
+
+    public ConnectionUser() {
+    }
+
+	public void destroy() {
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        if(req.getSession().getAttribute("user") == null) {
+        	res.sendRedirect(req.getContextPath()+"/user/connection");
+        	return;
+        } else {
+        	chain.doFilter(request, response);        	
+        }
+	}
+
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
