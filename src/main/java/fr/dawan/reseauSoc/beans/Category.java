3d9fd10fc30772bf8522de532623f11d5025e7d9
vianneyba <vianneyba@free.fr>
2018-09-16 11:22:11 +0200

    Ajout => bean Category

--- src/main/java/fr/dawan/reseauSoc/beans/Category.java
+++ src/main/java/fr/dawan/reseauSoc/beans/Category.java
@@ -1,143 +1,39 @@
 package fr.dawan.reseauSoc.beans;
 
-import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Category.
- */
-public class Category implements Serializable{
+@Entity
+@Table(name="Category")
+@PrimaryKeyJoinColumn(name="id")
+public class Category extends Likable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 844203126347475267L;
 
-	/** The id. */
 	private int id;
 	
-	/** The name. */
 	private String name;
 	
-	/** The parent. */
-	private Category parent;
-	
-
-	/**
-	 * Instantiates a new category.
-	 */
+	/* ****************************************************************************************
+	 * ****************************CONSTRUCTEUR************************************************
+	 * ***************************************************************************************/
 	public Category() {
 	
 	}
 
-	/**
-	 * Instantiates a new category.
-	 *
-	 * @param parent the parent
-	 */
-	public Category(Category parent) {
-		this.parent=parent;
-	}
-
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
+	/* ****************************************************************************************
+	 * ****************************GETTERS / SETTERS*******************************************
+	 * ***************************************************************************************/
 	public int getId() {
 		return id;
 	}
-
-	/**
-	 * Sets the id.
-	 *
-	 * @param id the new id
-	 */
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	/**
-	 * Gets the name.
-	 *
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
-
-	/**
-	 * Sets the name.
-	 *
-	 * @param name the new name
-	 */
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	/**
-	 * Gets the parent.
-	 *
-	 * @return the parent
-	 */
-	public Category getParent() {
-		return parent;
-	}
-
-	/**
-	 * Sets the parent.
-	 *
-	 * @param parent the new parent
-	 */
-	public void setParent(Category parent) {
-		this.parent = parent;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
-		return result;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Category other = (Category) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (parent == null) {
-			if (other.parent != null)
-				return false;
-		} else if (!parent.equals(other.parent))
-			return false;
-		return true;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", name=" + name + ", parent=" + parent + "]";
 	}
 }
